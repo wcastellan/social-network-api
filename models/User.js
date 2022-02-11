b72823f40589dfb26680e3258efcdb43b80422b9
Wyatt C <wcastellan@yahoo.com>
2022-02-11 14:36:54 -0500

    add npm init, download mongoose and express, create controllers folder, work on User.js file

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+           type: String,
+           unique: true,
+           required: 'Please enter a username',
+           trim: true 
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: 'Please enter a valid email',
+            match: [/.+\@.+\..+/]
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Users'
+        }]
+    },
+    {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+// length of user friends array
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
